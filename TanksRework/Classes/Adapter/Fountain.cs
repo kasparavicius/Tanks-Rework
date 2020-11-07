4210df2d0fdb03d8072a9a9b65a93de781f89780
UgniusBrO <ugnius.mockus@gmail.com>
2020-11-07 19:51:41 +0200

    Adapter client side
    Adapter client side implemented

---
+++ TanksRework/Classes/Adapter/Fountain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TanksRework.Classes.Adapter
+{
+    public interface Fountain
+    {
+        public int Heal();
+    }
+}
---
+++ TanksRework/Classes/Adapter/RestoreFountain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace TanksRework.Classes.Adapter
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    class RestoreFountain
+    {
+        [JsonProperty]
+        private string _id { get; set; }
+        [JsonProperty]
+        public int HealAmount { get; set; }
+        [JsonProperty]
+        public int positionx { get; set; }
+        [JsonProperty]
+        public int positiony { get; set; }
+        public RestoreFountain(string id, int heal, int posx, int posy)
+        {
+            _id = id;
+            HealAmount = heal;
+            positionx = posx;
+            positiony = posy;
+        }
+        public RestoreFountain()
+        {
+
+        }
+        public int Restore()
+        {
+            return HealAmount = 10;
+        }
+    }
+}
